90021b821efadda95fc83053c39a06d8af6ec826
Maximo Bautista <max.bautista97@gmail.com>
2018-12-25 18:07:53 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -8,7 +8,7 @@ def my_select(collection)
         selected.push(collection[i])
       end
         i += 1
-      end
+    end
     selected
   else
     puts "That collection is empty please add some items!"
