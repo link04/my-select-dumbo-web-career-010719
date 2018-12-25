85b11d6fb5e8668c6dd7c8c977462b5cd3d584ab
Maximo Bautista <max.bautista97@gmail.com>
2018-12-25 18:04:04 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,10 +1,10 @@
 def my_select(collection)
  if block_given?
    if collection.length > 0
-  i = 0 
-  while i < collection.length 
-    collection[0]
-  end
+    i = 0 
+    while i < collection.length 
+      collection[0]
+    end
   else
     puts "That collection is empty please add some items!"
   end
