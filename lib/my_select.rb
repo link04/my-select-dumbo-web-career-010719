ac7ea8ad132dc791cc9db85ee3bcf8931f86ba07
Maximo Bautista <max.bautista97@gmail.com>
2018-12-25 18:05:11 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -14,7 +14,7 @@ def my_select(collection)
  end
 end
 
-hello_t(["Tim", "Tom", "Jim"]) do |name|
+my_select(["Tim", "Tom", "Jim"]) do |name|
   if name.start_with?("T")
     puts "Hi, #{name}"
   end
