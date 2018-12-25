c02a6c3651ca55ca7fc9a419b373d77ee809af37
Maximo Bautista <max.bautista97@gmail.com>
2018-12-25 18:02:13 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,10 @@
 def my_select(collection)
- 
+ if block_given?
+  i = 0 
+  while i < collection.length 
+    collection[0]
+  end
+ else
+   puts "No block was given, but thast okay I've got your back."
+ end
 end
